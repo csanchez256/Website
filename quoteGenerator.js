be4b4acc680f8690919d0b9478342b14fdf8914c
Chris <csanchez505@gmail.com>
2017-05-21 04:16:58 -0600

    fixed the background resizing problem

---
+++ quoteGenerator.js
@@ -0,0 +1,7 @@
+
+
+var quotes = [
+  "alrighty thennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
+  "here's johnnyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
+  "you talkin to meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee?"
+];
