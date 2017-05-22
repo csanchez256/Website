d41c7bc8236c574c5de9017eb9ea6bc8c59ea319
Christopher Santos Sanchez <csanch87@unm.edu>
2017-05-22 16:07:04 -0600

    javascript quote generator now works

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,7 +1,8 @@
 
+/* Christopher Sanchez; 2017 */
 
 var quotes = [
-  "alrighty thennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
-  "here's johnnyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy",
-  "you talkin to meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee?"
+  ' \" A bird sitting on a tree branch is never <br> afraid of the branch breaking, because <br> her trust is not on the branch, but on <br> her own wings.\" -? ',
+  ' \" Find what you love and let it kill you."\ -Charles Bukowski ',
+  ' \" Where your talents and the needs of the world cross; <br> there lies your vocation."\ -Aristotle'
 ];
