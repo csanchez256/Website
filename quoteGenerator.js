ccfe5911f6ebe39ad7a0f65167271f157bb78b64
Christopher Santos Sanchez <csanch87@unm.edu>
2017-05-22 16:16:46 -0600

    fixed a minor text issue

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -2,7 +2,7 @@
 /* Christopher Sanchez; 2017 */
 
 var quotes = [
-  ' \" A bird sitting on a tree branch is never <br> afraid of the branch breaking, because <br> her trust is not on the branch, but on <br> her own wings.\" -? ',
-  ' \" Find what you love and let it kill you."\ -Charles Bukowski ',
-  ' \" Where your talents and the needs of the world cross; <br> there lies your vocation."\ -Aristotle'
+  ' \" A bird sitting on a tree branch is never <br> afraid of the branch breaking, because <br> her trust is not on the branch, but on <br> her own wings. \" -? ',
+  ' \" Find what you love and let it kill you. "\ -Charles Bukowski ',
+  ' \" Where your talents and the needs of the world cross; <br> there lies your vocation. "\ -Aristotle'
 ];
